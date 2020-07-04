9d0cc12b1aa7a0cb55631301829b3267f29c3fe0
colbyfayock <fayock@gmail.com>
2020-07-03 22:47:36 -0400

    moving to gatsby

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { FaGamepad } from 'react-icons/fa';
+
+import Container from 'components/Container';
+
+const Header = () => {
+  return (
+    <header id="header" className="container">
+      <Container>
+        <h1>
+          <FaGamepad />
+          Couch Co-Op Games
+        </h1>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,167 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import Layout from 'components/Layout';
+import Container from 'components/Container';
+
+const IndexPage = () => {
+  return (
+    <Layout pageName="home">
+      <Helmet>
+        <title>Couch Co-Op Games | Bringing back IRL video game campaigning</title>
+        <meta name="description" content="Awesome local cooperative video games perfect for the couch" />
+      </Helmet>
+      <Container>
+
+        <article id="halo" className="game">
+
+          <header>
+            <div className="game-header">
+              <h2 className="content">
+                Halo: The Master Chief Collection
+              </h2>
+            </div>
+          </header>
+
+          <div className="content">
+
+            <aside>
+              <div className="game-art">
+                <a href="https://amazon.com/Halo-Master-Chief-Collection-Xbox-One/dp/B00KSQHX1K?tag=ccgames-20">
+                  <img width="300" height="390" src="https://res.cloudinary.com/fay/image/upload/v1497208116/halo-boxart-300x390_kjqm59.jpg" data-hippi="https://res.cloudinary.com/fay/image/upload/v1497208117/halo-boxart-600x780_auhduk.jpg" alt="Halo: The Master Chief Collection Box Art" />
+                </a>
+              </div>
+              <p className="game-cta">
+                <a className="button button-large button-full button-primary" href="https://amazon.com/Halo-Master-Chief-Collection-Xbox-One/dp/B00KSQHX1K?tag=ccgames-20">
+                  <i className="fa fa-amazon icon-left" aria-hidden="true"></i> Buy on Amazon
+                </a>
+              </p>
+            </aside>
+
+            <div className="game-details">
+
+              <div className="game-details-meta">
+                <ul>
+                  <li>
+                    <strong>Local Co-Op:</strong> 2 Players
+                  </li>
+                  <li>
+                    <strong>Platform:</strong> Xbox One
+                  </li>
+                  <li>
+                    <strong>Release Date:</strong> November 11, 2014
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <strong>Publisher:</strong> <a href="https://www.microsoftstudios.com/">Microsoft Studios</a>
+                  </li>
+                  <li>
+                    <strong>Rating:</strong> <a href="https://www.esrb.org/ratings/ratings_guide.aspx">Mature 17+</a>
+                  </li>
+                </ul>
+              </div>
+
+              <p>
+                Save humanity with Master Chief through his epic journey to end an interstellar war.
+              </p>
+              <p>
+                Play through the entire story including a remastered
+                <a href="https://amazon.com/Halo-Combat-Evolved-Anniversary/dp/B0050SYY5E/?tag=ccgames-20"><strong>Halo: Combat Evolved Anniversary</strong> (Xbox 360)</a>
+                and <a href="https://amazon.com/Halo-2-Xbox/dp/B00008J7NZ?tag=ccgames-20"><strong>Halo 2: Anniversary</strong> (Xbox)</a>,
+                along with <a href="https://amazon.com/Halo-3-Xbox-360/dp/B000FRU0NU?tag=ccgames-20"><strong>Halo 3</strong> (Xbox 360)</a>
+                and <a href="https://amazon.com/Halo-4-Xbox-360-Standard-Game/dp/B0050SYX8W?tag=ccgames-20"><strong>Halo 4</strong> (Xbox 360)</a>.
+              </p>
+
+              <p className="note">
+                Note: The latest chapter <a href="https://amazon.com/Halo-5-Guardians/dp/B00DB9JV5W?tag=ccgames-20"><strong>Halo 5: Guardians</strong> (Xbox One)</a>
+                is not included and does not offer local co-op campaign.
+                Additionally, Halo 5 requires multiple Xbox One consoles connected
+                through Xbox Live.
+              </p>
+
+            </div>
+
+          </div>
+
+        </article>
+
+        <article id="gears-of-war" className="game">
+
+          <header>
+            <div className="game-header">
+              <h2 className="content">
+                Gears of War 4
+              </h2>
+            </div>
+          </header>
+
+          <div className="content">
+            <aside>
+              <div className="game-art">
+                <a href="https://amazon.com/Gears-War-4-Xbox-One/dp/B01MFAM0RU?tag=ccgames-20">
+                  <img width="300" height="390" src="https://res.cloudinary.com/fay/image/upload/v1505090222/gears_of_war-boxart-300x390_wjj6kb.jpg" data-hippi="https://res.cloudinary.com/fay/image/upload/v1505090222/gears_of_war-boxart-600x780_ke1ce2.jpg" alt="Gears of War 4 Box Art" />
+                </a>
+              </div>
+              <p className="game-cta">
+                <a className="button button-large button-full button-primary" href="https://amazon.com/Gears-War-4-Xbox-One/dp/B01MFAM0RU?tag=ccgames-20">
+                  <i className="fa fa-amazon icon-left" aria-hidden="true"></i> Buy on Amazon
+                </a>
+              </p>
+            </aside>
+
+            <div className="game-details">
+
+              <div className="game-details-meta">
+                <ul>
+                  <li>
+                    <strong>Local Co-Op:</strong> 2 Players
+                    </li>
+                  <li>
+                    <strong>Platform:</strong> Xbox One
+                    </li>
+                  <li>
+                    <strong>Release Date:</strong> October 11, 2016
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <strong>Publisher:</strong> <a href="https://www.microsoftstudios.com/">Microsoft Studios</a>
+                  </li>
+                  <li>
+                    <strong>Rating:</strong> <a href="https://www.esrb.org/ratings/ratings_guide.aspx">Mature 17+</a>
+                  </li>
+                </ul>
+              </div>
+
+              <p>
+                Rip through this latest chapter of one of the most acclaimed video game franchises in history
+                as you save the planet from monstrous creatures.
+              </p>
+
+              <p>
+                Rather play the whole story? Start off with the beginning of the story with
+                the remastered
+                <a href="https://amazon.com/Gears-War-Ultimate-Xbox-One/dp/B00ZMBLKPG?tag=ccgames-20">Gear of War: Ultimiate Edition</a>
+                (<a href="https://amazon.com/Gears-War-Xbox-360/dp/B000FRS9II?tag=ccgames-20">original available for Xbox 360</a>),
+                <a href="https://amazon.com/Gears-War-2-Xbox-360/dp/B000ZK9QD2?tag=ccgames-20">Gears of War 2</a>, and
+                <a href="https://amazon.com/Gears-War-3/dp/B002I0H79C?tag=ccgames-20">Gears of War 3</a>.
+              </p>
+
+              <p className="note">
+                Looking to save a few bucks? Try getting the <a href="https://amazon.com/Gears-War-Triple-Pack-Xbox-Bundle/dp/B004JSDQPU?tag=ccgames-20">Gears of War Triple Pack</a>
+                featuring Gears of War 1-2 and the Gears of War 2 All Fronts Pack!
+              </p>
+
+            </div>
+
+          </div>
+
+        </article>
+
+      </Container>
+    </Layout>
+  );
+};
+
+export default IndexPage;
